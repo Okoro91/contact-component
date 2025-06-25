a1cf0b40ddad460094d0df3c65e23830f7cb0e86
okoro91 <mi.fracri91@gmail.com>
2025-06-25 23:17:44 +0100

    optimized code with props

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,57 +1,52 @@
+import ContactCard from "./ContactCard";
+
 const Contact = () => {
   return (
     <div className="contacts">
-      <article className="contact-card">
-        <img src="/mr-whiskerson.png" alt="Photo of Mr. Whiskerson" />
-        <h3>Mr. Whiskerson</h3>
-        <div className="info-group">
-          <img src="/phone-icon.png" alt="phone icon" />
-          <p>(212) 555-1234</p>
-        </div>
-        <div className="info-group">
-          <img src="/mail-icon.png" alt="mail icon" />
-          <p>mr.whiskaz@catnap.meow</p>
-        </div>
-      </article>
-
-      <article className="contact-card">
-        <img src="/fluffykins.png" alt="Photo of Fluffykins" />
-        <h3>Fluffykins</h3>
-        <div className="info-group">
-          <img src="/phone-icon.png" alt="phone icon" />
-          <p>(212) 555-2345</p>
-        </div>
-        <div className="info-group">
-          <img src="/mail-icon.png" alt="mail icon" />
-          <p>fluff@me.com</p>
-        </div>
-      </article>
-
-      <article className="contact-card">
-        <img src="/felix.png" alt="Photo of Felix" />
-        <h3>Felix</h3>
-        <div className="info-group">
-          <img src="/phone-icon.png" alt="phone icon" />
-          <p>(212) 555-4567</p>
-        </div>
-        <div className="info-group">
-          <img src="/mail-icon.png" alt="mail icon" />
-          <p>thecat@hotmail.com</p>
-        </div>
-      </article>
-
-      <article className="contact-card">
-        <img src="/pumpkin.png" alt="Photo of Pumpkin" />
-        <h3>Pumpkin</h3>
-        <div className="info-group">
-          <img src="/phone-icon.png" alt="phone icon" />
-          <p>(0800) CAT KING</p>
-        </div>
-        <div className="info-group">
-          <img src="/mail-icon.png" alt="mail icon" />
-          <p>pumpkin@scrimba.com</p>
-        </div>
-      </article>
+      <ContactCard
+        img="/mr-whiskerson.png"
+        alt="Photo of Mr. Whiskerson"
+        name="Mr. Whiskerson"
+        numIcon="/phone-icon.png"
+        numalt="phone icon"
+        num="(212) 555-1234"
+        mailIcon="/mail-icon.png"
+        mailalt="mail icon"
+        mail="mr.whiskaz@catnap.meow"
+      />
+      <ContactCard
+        img="/fluffykins.png"
+        alt="Photo of Fluffykins"
+        name="Fluffykins"
+        numIcon="/phone-icon.png"
+        numalt="phone icon"
+        num="(212) 555-2345"
+        mailIcon="/mail-icon.png"
+        mailalt="mail icon"
+        mail="fluff@me.com"
+      />
+      <ContactCard
+        img="/felix.png"
+        alt="Photo of Felix"
+        name=" Alhaji Felix"
+        numIcon="/phone-icon.png"
+        numalt="phone icon"
+        num="(212) 555-4567"
+        mailIcon="/mail-icon.png"
+        mailalt="mail icon"
+        mail="thecat@hotmail.com"
+      />
+      <ContactCard
+        img="/pumpkin.png"
+        alt="Photo of Pumpkin"
+        name="Chief Pumpkin"
+        numIcon="/phone-icon.png"
+        numalt="phone icon"
+        num="(0800) CAT KING"
+        mailIcon="/mail-icon.png"
+        mailalt="mail icon"
+        mail="pumpkin@hotmeow.com"
+      />
     </div>
   );
 };
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,5 +1,18 @@
-const contactCard = () => {
-  return <div>contact-card</div>;
+const contactCard = (props) => {
+  return (
+    <article className="contact-card">
+      <img src={props.img} alt={props.alt} />
+      <h3>{props.name}</h3>
+      <div className="info-group">
+        <img src={props.numIcon} alt={props.numalt} />
+        <p>{props.num}</p>
+      </div>
+      <div className="info-group">
+        <img src={props.mailIcon} alt={props.mailalt} />
+        <p>{props.mail}</p>
+      </div>
+    </article>
+  );
 };
 
 export default contactCard;
