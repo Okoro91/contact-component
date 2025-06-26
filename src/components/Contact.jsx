99b95882fc6194752caedee5d7c9fe93f2205604
okoro91 <mi.fracri91@gmail.com>
2025-06-26 07:56:25 +0100

    improved image display

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,46 +4,46 @@ const Contact = () => {
   return (
     <div className="contacts">
       <ContactCard
-        img="/mr-whiskerson.png"
+        img="./mr-whiskerson.png"
         alt="Photo of Mr. Whiskerson"
         name="Mr. Whiskerson"
-        numIcon="/phone-icon.png"
+        numIcon="./phone-icon.png"
         numalt="phone icon"
         num="(212) 555-1234"
-        mailIcon="/mail-icon.png"
+        mailIcon="./mail-icon.png"
         mailalt="mail icon"
         mail="mr.whiskaz@catnap.meow"
       />
       <ContactCard
-        img="/fluffykins.png"
+        img="./fluffykins.png"
         alt="Photo of Fluffykins"
         name="Fluffykins"
-        numIcon="/phone-icon.png"
+        numIcon="./phone-icon.png"
         numalt="phone icon"
         num="(212) 555-2345"
-        mailIcon="/mail-icon.png"
+        mailIcon="./mail-icon.png"
         mailalt="mail icon"
         mail="fluff@me.com"
       />
       <ContactCard
-        img="/felix.png"
+        img="./felix.png"
         alt="Photo of Felix"
         name=" Alhaji Felix"
-        numIcon="/phone-icon.png"
+        numIcon="./phone-icon.png"
         numalt="phone icon"
         num="(212) 555-4567"
-        mailIcon="/mail-icon.png"
+        mailIcon="./mail-icon.png"
         mailalt="mail icon"
         mail="thecat@hotmail.com"
       />
       <ContactCard
-        img="/pumpkin.png"
+        img="./pumpkin.png"
         alt="Photo of Pumpkin"
         name="Chief Pumpkin"
-        numIcon="/phone-icon.png"
+        numIcon="./phone-icon.png"
         numalt="phone icon"
         num="(0800) CAT KING"
-        mailIcon="/mail-icon.png"
+        mailIcon="./mail-icon.png"
         mailalt="mail icon"
         mail="pumpkin@hotmeow.com"
       />
